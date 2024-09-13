caf5f2d3d129a243278c2e2df61604c6d6dc0b8e
Pradeep1234566 <pawarpradeep1707@gmail.com>
2024-09-13 18:07:56 +0530

    Time: 1474 ms (5.01%), Space: 15.9 MB (67.43%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -6,16 +6,24 @@ class Solution(object):
         """
         count_5 = 0
         count_10 = 0
-        count_20 = 0
-        for i in bills:
-            if i == 5:
+        
+        for bill in bills:
+            if bill == 5:
                 count_5 += 1
-            elif i == 10:
+            elif bill == 10:
                 count_10 += 1
-                if count_5 < 1:
+                if count_5 > 0:
+                    count_5 -= 1
+                else:
                     return False
-            elif i == 20:
-                if count_5 < 3 or (count_10<1 and count_5<1):
+            elif bill == 20:
+                # Prioritize giving a 10 and a 5 as change, if possible
+                if count_10 > 0 and count_5 > 0:
+                    count_10 -= 1
+                    count_5 -= 1
+                # Otherwise, try giving three 5s as change
+                elif count_5 >= 3:
+                    count_5 -= 3
+                else:
                     return False
         return True
-        
