95cbe342f36464ce76a11f539fbcb75edab7bbb6
Pradeep1234566 <pawarpradeep1707@gmail.com>
2024-09-16 21:25:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        answer = []
+        key = 0
+        if x < 0:
+            key = 1
+            x = abs(x)
+        while x:
+            answer.append(x%10)
+            x = x // 10
+        answer.reverse()
+        # print(answer)
+        total = 0
+        for i in range(len(answer)):
+            k = (10**i) * answer[i]
+            total += k
+        # print(total)
+        if key == 1:
+            return (-1) * total
+        else:
+            return total
