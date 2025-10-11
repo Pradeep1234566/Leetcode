be52312162b79afa3044f727590fca173dc746c3
Pradeep Pawar <pawarpradeep1707@gmail.com>
2025-10-11 16:11:14 +0530

    Time: 0 ms (100%), Space: 12.3 MB (82.82%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,43 @@
+class Solution(object):
+
+    def helper(self, index,  digits, mapping, result, temp):
+        if index == len(digits):
+            result.append(''.join(temp))
+            return
+        
+        letters = mapping[digits[index]]
+
+        for character in letters:
+            temp.append(character)
+            self.helper(index+1, digits, mapping, result, temp)
+            temp.pop()
+        
+
+
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+
+        mapping = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+        }
+        result = []
+        temp = []
+        index = 0
+        if len(digits) == 0:
+            return result
+
+        self.helper(index,  digits, mapping, result, temp)
+        return result
+        
+
+        
