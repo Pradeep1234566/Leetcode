bfe7a8124181a6cceaff8e621eca1c69169102a0
Pradeep Pawar <pawarpradeep1707@gmail.com>
2025-12-03 09:38:49 +0530

    Time: 536 ms (59.55%), Space: 33.8 MB (36.5%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,19 +1,4 @@
 class Solution(object):
-    def recur(self, text1, text2, n, m, dp):
-        if n == 0 or m == 0:
-            return 0
-        
-        if dp[n][m] != -1:
-            return dp[n][m]
-
-        if text1[n-1] == text2[m-1]:
-            dp[n][m] = 1 + self.recur(text1, text2, n-1, m-1, dp)
-        else:
-            dp[n][m] = max(self.recur(text1, text2, n-1, m, dp),
-                           self.recur(text1, text2, n, m-1, dp))
-        
-        return dp[n][m]
-        
     def longestCommonSubsequence(self, text1, text2):
         """
         :type text1: str
@@ -23,5 +8,15 @@ class Solution(object):
         n = len(text1)
         m = len(text2)
 
-        dp = [[-1] * (m+1) for _ in range(n+1)]
-        return self.recur(text1, text2, n, m, dp)
+        dp = [[0] * (m+1) for _ in range(n+1)]
+
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+        
+        return dp[n][m]
+
+
