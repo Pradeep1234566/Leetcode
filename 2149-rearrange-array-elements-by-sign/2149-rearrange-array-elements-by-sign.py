a3ea10354e31cf0e83d471f3ddf98354366ba9b3
Pradeep Pawar <pawarpradeep1707@gmail.com>
2025-12-04 14:24:55 +0530

    Time: 67 ms (78.35%), Space: 47.8 MB (39.09%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,24 +1,24 @@
 class Solution(object):
     def rearrangeArray(self, nums):
         """
-        Rearrange the array so that every consecutive pair has opposite signs.
         :type nums: List[int]
         :rtype: List[int]
         """
-        positive = []
-        negative = []
-        
-        # Separate positive and negative numbers
-        for num in nums:
-            if num > 0:
-                positive.append(num)
+        n = len(nums)
+        oddIndex = 1
+        evenIndex = 0
+        result = [0] * (n)
+
+        for i in range(n):
+            if nums[i] < 0:
+                result[oddIndex] = nums[i]
+                oddIndex += 2
+            
             else:
-                negative.append(num)
-        
-        # Merge the lists alternately
-        result = []
-        for pos, neg in zip(positive, negative):
-            result.append(pos)
-            result.append(neg)
+                result[evenIndex] = nums[i]
+                evenIndex += 2
         
         return result
+
+
+        
