c97b3a69402b38b92c88fd3fe37a19af00f9cc1c
Pradeep Pawar <pawarpradeep1707@gmail.com>
2025-12-19 10:35:22 +0530

    Time: 0 ms (100%), Space: 13.4 MB (29.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,22 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
+        
         """
-        indexs = {}
 
-      
+
+        indexs = {}
         n = len(nums)
 
         for i in range(n):
-            indexs[nums[i]] = i
-        
-        for i in range(n):
             difference = target - nums[i]
-            if difference in indexs and indexs[difference] != i:
-                return [i, indexs[difference]]
-                break
-        
-        return []
-        
-        
-        
+
+            if difference in indexs:
+                return [indexs[difference], i]
+            
+            else:
+                indexs[nums[i]] = i
+
+    
