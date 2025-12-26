fd0e001b7cfd24c5598d5a15c7d6c95828bb714e
Pradeep Pawar <pawarpradeep1707@gmail.com>
2025-12-26 10:55:16 +0530

    Time: 2 ms (9.07%), Space: 12.6 MB (12.3%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,41 +1,28 @@
 class Solution(object):
-
-    def helper(self, index,  digits, mapping, result, temp):
+    def helper(self, index, digits, mapping, result, curr):
         if index == len(digits):
-            result.append(''.join(temp))
+            result.append(curr)
             return
-        
-        letters = mapping[digits[index]]
 
+        letters = mapping[digits[index]]
         for letter in letters:
-            temp.append(letter)
-            self.helper(index+1, digits, mapping, result, temp)
-            temp.pop()
-        
+            self.helper(index + 1, digits, mapping, result, curr + letter)
+
     def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
+        if not digits:
+            return []
 
         mapping = {
-        '2': 'abc',
-        '3': 'def',
-        '4': 'ghi',
-        '5': 'jkl',
-        '6': 'mno',
-        '7': 'pqrs',
-        '8': 'tuv',
-        '9': 'wxyz'
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
         }
+
         result = []
-        temp = []
-        index = 0
-        if len(digits) == 0:
-            return result
-
-        self.helper(index,  digits, mapping, result, temp)
+        self.helper(0, digits, mapping, result, "")
         return result
-        
-
-        
