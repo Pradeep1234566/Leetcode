4ae3cf53eac0056e0ba5f1699e73a29ed541e38a
Pradeep Pawar <pawarpradeep1707@gmail.com>
2026-01-01 08:03:47 +0530

    Time: 0 ms (100%), Space: 12.4 MB (81.81%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -4,22 +4,19 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        k = 0
+        digit = 0
         n = len(digits)
+
         for i in range(n):
-            m = digits[i] * (10**(n-i-1))
-            #print(m)  # This will print each intermediate result
-            k += m
-        #print(k)  # This will print the final number
-        k = k + 1
-        answer = []
-        print(k)
-        while(k):
-            answer.append(k%10)
-            k = k / 10
-        answer.reverse()
-        return answer
-
-
-
+            digit = digit + (digits[n-i-1]*(10**(i)))
         
+        
+        digit += 1
+        result = []
+
+        while digit:
+            temp = digit % 10
+            result.append(temp)
+            digit = digit // 10
+        
+        return result[::-1]
