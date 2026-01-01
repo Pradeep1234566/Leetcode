7c4824b7e34387854d920ecb18ebc5c2e6fd9fd0
Pradeep Pawar <pawarpradeep1707@gmail.com>
2026-01-01 08:11:49 +0530

    Time: 0 ms (100%), Space: 12.5 MB (11.43%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -4,19 +4,12 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        digit = 0
         n = len(digits)
 
-        for i in range(n):
-            digit = digit + (digits[n-i-1]*(10**(i)))
+        for i in range(n-1,-1,-1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
         
-        
-        digit += 1
-        result = []
-
-        while digit:
-            temp = digit % 10
-            result.append(temp)
-            digit = digit // 10
-        
-        return result[::-1]
+        return [1] + digits
