34d460e786c8946762186b38483d13833e1a56bb
Pradeep Pawar <pawarpradeep1707@gmail.com>
2026-03-03 09:16:23 +0530

    Time: 0 ms (100%), Space: 12.5 MB (37.39%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,15 +1,27 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+
+        Longest Prefix means it should start from index 0
+        
+        """
+        if not strs:
+            return ""
+        
         min_length = float('inf')
-        s =[]
-        for s in strs:
-            if min_length > len(s):
-                min_length = len(s)
+
+        for string in strs:
+            n = len(string)
+            min_length = min(min_length, n)
+        
         i = 0
+
         while i < min_length:
-            for s in strs:
-                if s[i]!=strs[0][i]:
-                    return s[:i]
-            i = i + 1
-        return s[:i]
-            
+            for string in strs:
+                if string[i] != strs[0][i]:
+                    return strs[0][:i]
+            i += 1
+        
+        return strs[0][:min_length]
