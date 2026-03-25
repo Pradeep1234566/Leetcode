45d331815ea731e59b2897491115d251e15b06d7
Pradeep Pawar <pawarpradeep1707@gmail.com>
2026-03-25 09:00:29 +0530

    Time: 0 ms (100%), Space: 12.3 MB (77.7%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,28 +1,29 @@
 class Solution(object):
-    def helper(self, index, digits, mapping, result, curr):
+    def helper(self, index, digits, mapping, temp, result):
         if index == len(digits):
-            result.append(curr)
+            result.append(temp)
             return
 
         letters = mapping[digits[index]]
+
         for letter in letters:
-            self.helper(index + 1, digits, mapping, result, curr + letter)
+            self.helper(index + 1, digits, mapping, temp + letter, result)
 
     def letterCombinations(self, digits):
-        if not digits:
-            return []
+       mapping = {
+        '2' : 'abc',
+        '3' : 'def',
+        '4' : 'ghi',
+        '5' : 'jkl',
+        '6' : 'mno',
+        '7' : 'pqrs',
+        '8' : 'tuv',
+        '9' : 'wxyz'
+       }
+       temp = []
+       result = []
+       index = 0
+       self.helper(index, digits, mapping, '', result)
+       return result
 
-        mapping = {
-            '2': 'abc',
-            '3': 'def',
-            '4': 'ghi',
-            '5': 'jkl',
-            '6': 'mno',
-            '7': 'pqrs',
-            '8': 'tuv',
-            '9': 'wxyz'
-        }
-
-        result = []
-        self.helper(0, digits, mapping, result, "")
-        return result
+       
