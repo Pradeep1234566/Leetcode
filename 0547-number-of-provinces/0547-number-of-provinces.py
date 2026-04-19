863e5ccab7b9ffa36e6a17ec59b9384a9dbea2a5
Pradeep Pawar <pawarpradeep1707@gmail.com>
2026-04-19 11:49:08 +0530

    Time: 11 ms (46.43%), Space: 12.9 MB (76.79%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,23 +1,31 @@
 class Solution(object):
-    def dfs(self, node, visited, adjacency_list):
-        visited[node] = 1
-        for val in adjacency_list[node]:
-            if visited[val] != 1:
-                self.dfs(val, visited, adjacency_list)
-
-    def findCircleNum(self, isConnected):
-        n = len(isConnected)
-        adjacency_list = [[] for _ in range(n)]
-        for i in range(n):
-            for j in range(n):
-                if isConnected[i][j] == 1 and i != j:
-                    adjacency_list[i].append(j)
-                    adjacency_list[j].append(i)
+    def helper(self, adj_list, visited, node):
+        visited.add(node)
+        for neighbours in adj_list[node]:
+            if neighbours not in visited:
+                self.helper(adj_list, visited, neighbours)
         
-        visited = [0] * n
+    
+    def findCircleNum(self, isConnected):
+        V = len(isConnected)
+        
+        adj_list = [[] for _ in range(V)]
+        
+        for i in range(V):
+            for j in range(V):
+                if isConnected[i][j] == 1 and i != j:
+                    adj_list[i].append(j)
+                    adj_list[j].append(i)
+        
+
+        visited = set()
         count = 0
-        for i in range(n):
-            if visited[i] != 1:
-                self.dfs(i, visited, adjacency_list)
+
+        for i in range(V):
+            if i not in visited:
+                self.helper(adj_list, visited, i)
                 count += 1
+    
         return count
+
+        
